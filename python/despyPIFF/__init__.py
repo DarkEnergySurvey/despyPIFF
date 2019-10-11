9896452e2bc876f5be6a95aceb89ddcb76e99dca
Robert Gruendl <gruendl@illinois.edu>
2019-10-11 19:20:21 +0000

    Update to incorporate SVN keywords

--- python/despyPIFF/__init__.py
+++ python/despyPIFF/__init__.py
@@ -1,7 +1,7 @@
-# $Id: __init__.py 40858 2015-11-19 17:36:27Z felipe $
-# $Rev:: 40858                            $:  # Revision of last commit.
-# $LastChangedBy:: felipe                 $:  # Author of last commit.
-# $LastChangedDate:: 2015-11-19 11:36:27 #$:  # Date of last commit.
+# $Id$
+# $Rev::                                  $:  # Revision of last commit.
+# $LastChangedBy::                        $:  # Author of last commit.
+# $LastChangedDate::                      $:  # Date of last commit.
 
 __author__  = "Robert Gruendl"
 __version__ = '1.0.0'
