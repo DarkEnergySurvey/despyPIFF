797355e4c40f68b0204517e26a01f16104c38214
Robert Gruendl <gruendl@illinois.edu>
2019-10-11 19:19:48 +0000

    Added python init file

---
+++ python/despyPIFF/__init__.py
@@ -0,0 +1,13 @@
+# $Id: __init__.py 40858 2015-11-19 17:36:27Z felipe $
+# $Rev:: 40858                            $:  # Revision of last commit.
+# $LastChangedBy:: felipe                 $:  # Author of last commit.
+# $LastChangedDate:: 2015-11-19 11:36:27 #$:  # Date of last commit.
+
+__author__  = "Robert Gruendl"
+__version__ = '1.0.0'
+version = __version__
+
+"""
+ A collection of pure Python functions used within despyPIFF
+"""
+
