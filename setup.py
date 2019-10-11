2388c396c4eac9917cbd2b698211e83fe0d621e1
Felipe Menanteau <felipe@illinois.edu>
2019-10-11 20:30:25 +0000

    Add setup.py to PIFF

---
+++ setup.py
@@ -0,0 +1,19 @@
+import distutils
+from distutils.core import setup
+import glob
+
+bin_files = glob.glob("bin/*.py")
+
+# The main call
+setup(name='despyPIFF',
+      version ='1.0.0',
+      license = "GPL",
+      description = "DESDM tools for PIFF",
+      author = "Robert Gruendl",
+      author_email = "gruendl@illinois.edu",
+      packages = ['despyPIFF'],
+      package_dir = {'': 'python'},
+      scripts = bin_files,
+      data_files=[('ups',['ups/despyPIFF.table'])],
+      )
+
