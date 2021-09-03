fc1970de79f740c05d98f9fadcd27c928ea75f33
Felipe Menanteau <felipe@illinois.edu>
2021-09-03 13:01:53 -0500

    Updating version to 3.0.1

--- python/despyPIFF/__init__.py
+++ python/despyPIFF/__init__.py
@@ -4,7 +4,7 @@
 # $LastChangedDate::                      $:  # Date of last commit.
 
 __author__  = "Robert Gruendl"
-__version__ = '3.0.0'
+__version__ = '3.0.1'
 version = __version__
 
 """
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ bin_files = glob.glob("bin/*.py")
 
 # The main call
 setup(name='despyPIFF',
-      version ='3.0.0',
+      version ='3.0.1',
       license = "GPL",
       description = "DESDM tools for PIFF",
       author = "Robert Gruendl",
